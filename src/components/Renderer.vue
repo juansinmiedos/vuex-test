be5dd3f0e9e6d348ec3cc064df06fd922e82cfb9
juan <juanvidaldom@gmail.com>
2021-03-08 16:10:40 -0600

    Renderer listens to brother event

--- src/components/Renderer.vue
+++ src/components/Renderer.vue
@@ -1,9 +1,32 @@
 <template>
-  <div>Renderer</div>
+  <div>
+    Renderer
+
+    <div v-for="(delivery, key) in deliveries" :key="key">
+      <div>{{ delivery.customerName }}</div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'Renderer'
+  name: 'Renderer',
+
+  // data () {
+  //   return {
+  //     deliveries: [
+  //       {
+  //         id: 0,
+  //         customerName: 'Juan'
+  //       }
+  //     ]
+  //   }
+  // },
+
+  computed: {
+    deliveries () {
+      return this.$store.getters.getDeliveries
+    }
+  }
 }
 </script>
